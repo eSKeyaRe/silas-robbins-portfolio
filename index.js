b43aeb492282c46be0be5b07212577769785e15f
Silas Robbins <silaskalerobbins@gmail.com>
2022-11-15 15:39:18 -0800

    added projects and interactive splash

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 setTimeout(function() { 
     document.getElementsByClassName("name")[0].setAttribute("id", "animation")
  }, 1000);
+ const nameClick = () =>{
+    document.getElementById("details").style.display = "block"
+ }
